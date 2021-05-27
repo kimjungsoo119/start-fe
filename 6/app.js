61efc0a9ee6fd9ff4e7d1222675ea53bc55b00d1
김정수 <62883942+kimjungsoo119@users.noreply.github.com>
2021-05-28 08:18:36 +0900

    6

--- 6/app.js
+++ 6/app.js
@@ -1,66 +1,39 @@
-console.log('app');
+// let a = 'aa';
+// const b = 'bb';
+// var c = '11';
+// c = 33;
+// b = 11;
+// console.log(a, b);
 
-/* //Create
-var toods =['운동'];
-var todo = '게임';
-toods.push(todo);
-console.log(todos);
-//Read
-todos.forEach(function(todo){
-    console.log(todo);
-});
-//Update
-var arr = ['운동','게임'];
-var updateTodo = '게임';
-var updateIndex = todos.findIndex(function(todo) {
-  return todo === updateTodo;
-});
-todos[updateIndex] = '공부';
-console.log(todos);
-// Delete
-var todos = ['운동', '공부', '목욕'];
-var deleteTodo = '공부';
-var deleteIndex = todos.findIndex(function(todo) {
-  return todo === deleteTodo;
-});
-todos.splice(deleteIndex, 1);
-console.log(todos); */
 
-/* var isMan = ture;
-if(isMan){
-    console.log('1');
-}
-else if (!isMan && isHero){
-}
-else {
-    console.log('2');
-} */
 
-/* var count = [10,20,30,40,50,60,70,100,110,120];
-const result = count.reduce(function add(sum, currValue) {
-  return sum + currValue;
-}, 0);
-const average = result / count.length;
-console.log(average);  */
+// var str = 'aaaaaaa \
+// sssdfsd';
+// const url =`aaaaa
+//     cccc
+//     dddd`;
+// console.log(str,s);
 
-function sumInput() {
 
-    let numbers = [];
-  
-    while (true) {
-  
-      input  = prompt("더 할 숫자를 입력해 주 세요.", 0);
-  
-      if (value === "" || value === null || !isFinite(value));
-  
-      numbers.push(+value);
-    }
-  
-    let sum = 0;
-    for (let number of numbers) {
-      sum += number;
-    }
-    return sum;
-  }
-  
-sumInput();
+// function sum(x, y ) {
+//     return x + y;
+// }
+//
+// console.log(sum(1));
+
+
+// const arr = [1, 2, 3];
+//
+// const newArr = arr.map((item) => item + 1);
+// console.log(newArr);
+//
+// document.getElementById('box').addEventListener('click', event => console.log(1));
+//
+// function sum(x,y,z) {
+//     return x + y+ z;
+// }
+//
+const numbers = [1,2,3];
+const words = ['a','b'];
+const [c1, c2] = words;
+console.log(sum(1,2,3));//
