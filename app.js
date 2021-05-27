abff158307fce7c79dff40d64c097f97b16e4277
김정수 <62883942+kimjungsoo119@users.noreply.github.com>
2021-05-28 06:28:00 +0900

    g
    g

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.timeLog('app.js');
+public class googoodan{
+
+    static void startgoogoodan(int n){
+    for(int i =1; i<10 ; i++){
+     System.out.println(n+"*"+i+"="+(n*i));
+    }System.out.println();
+    }
+    public static void main(String[]args){
+    for(int i= 2; i<10; i++){
+    startgoogoodan(i);
+    }
+    }
+    }
