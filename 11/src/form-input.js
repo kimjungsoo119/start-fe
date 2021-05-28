db2fc9de8b132c70ea860c5f69b422f4313aa636
김정수 <62883942+kimjungsoo119@users.noreply.github.com>
2021-05-28 14:33:55 +0900

    todo

---
+++ 11/src/form-input.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>quiz</title>
+    <style>
+       
+  span {
+            width: 100px;
+            height: 100px;
+            display: block;
+            float: left;
+        }
+        .black {
+            background-color: rgba(0, 0, 0, 0.877);
+        }
+        .white {
+            background-color: rgb(255, 255, 255);
+        }
+        .select {
+            background-color: red;
+        }
+        #board {
+            width: 400px;
+            height: 400px;
+            border: solid 5px grey;
+          
+        }
+    </style>
+</head>
+<body>
+
+<div id="board"></div>
+<script src="quiz2.js"></script>
+</body>
+</html>
